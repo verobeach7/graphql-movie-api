e494f5825e82640c8240fb6ae03907c4a90eda6d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-21 00:24:28 +0900

    GraphQL Movie API: Non Nullable Fields

--- server.js
+++ server.js
@@ -1,39 +1,32 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// package.json에 "type": "module"을 추가하지 않으면 import 대신에 require()를 사용해줘야 함
-// const { ApolloServer } = require("apollo-server")
-// 위와 같이 사용하는 것보다 import를 사용하는 것이 좋음
-const graphql = String.raw;
-
+// !: Non Nullable Fields
+// !가 없으면 기본적으로 Nullable임. 즉, Null 값이 허용됨.
 const typeDefs = `#graphql
   type User{
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String # Nullable
   }
-  # allTweets를 위한 타입 생성
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    # allTweets에는 다양한 데이터가 들어가 있기 때문에 하나의 타입(Int, Float, String, ID 등)으로 설정 불가
-    # 이럴 때는 Type을 생성해줘야 함
-    allTweets: [Tweet]
-    # 수많은 트윗 중 어떤 트윗인지 알기 위해서 argument를 제공
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet # Nullable
   }
   type Mutation{
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean # 해당 트윗을 찾았으면 True, 없으면 False 반환
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean! 
   }
 `;
 
-// Create an instance of ApolloServer
 const server = new ApolloServer({ typeDefs });
 
-// 서버 실행
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
