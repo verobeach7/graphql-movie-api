174fd1579da3b5a697299b956d585017103c3229
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-20 18:52:48 +0900

    GraphQL Movie API: Scalar&Root Types

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 const graphql = String.raw;
 
 const typeDefs = `#graphql
-  type Query {
+  type User{
+    id: ID
+    username: String
+  }
+  # allTweets를 위한 타입 생성
+  type Tweet {
+    id: ID
     text: String
-    hello: String
+    author: User
+  }
+  type Query {
+    # allTweets에는 다양한 데이터가 들어가 있기 때문에 하나의 타입(Int, Float, String, ID 등)으로 설정 불가
+    # 이럴 때는 Type을 생성해줘야 함
+    allTweets: [Tweet]
+    # 수많은 트윗 중 어떤 트윗인지 알기 위해서 argument를 제공
+    tweet(id: ID): Tweet
   }
 `;
 
